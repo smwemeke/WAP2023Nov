e534e3ef542d6551efce06c79347edbd1b2d3dac
smwemeke <sue.mwemeke@gmail.com>
2023-11-05 16:51:49 -0500

    Lab5 Assignment

---
+++ Lab5/lab5.js
@@ -0,0 +1,30 @@
+
+// No.1
+console.log('Lab 5 N0.1')
+function sum(arr){
+    let sum =0;
+    for(let i=0; i<arr.length;i++){
+        if(arr[i] > 20){  
+        sum+=arr[i];
+    }
+}
+    return sum;
+ } 
+ const sumArray = [23,2,5,1,24];
+ const res = sum(sumArray);
+ console.log('Sum of elements greater that 20: ' + res);
+
+
+ //No.2
+console.log('Lab 5 N0.2')
+ const getNewArray = function(strArr){
+    const newArr = strArr.filter(function(str){
+        return str.length >=5 && str.includes('a');
+    });
+    return newArr;
+        } ;   
+ const finalArr = [ 'damian','alvin', 'aaron','suzan'];
+ console.log(getNewArray(finalArr));
+
+ //N0.3
+ console.log('Lab 5 N0.3')
