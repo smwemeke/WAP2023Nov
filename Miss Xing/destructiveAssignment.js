22904e2f550e5f4e19b7c277d718f9b9e0b7f022
smwemeke <sue.mwemeke@gmail.com>
2023-11-07 16:10:58 -0600

    changes

--- Miss Xing/destructiveAssignment.js
+++ Miss Xing/destructiveAssignment.js
@@ -1,4 +1,20 @@
 let countryNames = ['USA', 'Uganda', 'Kenya'];
 
 let [name1, name2] = countryNames;
-console.log(name1, name2);
+//console.log(name1, name2);
+
+
+
+const concat = (...args) => {
+    return args.reduce((result, arg) => {
+      if (typeof arg === 'string') {
+        result.push(...arg);
+      } else if (Array.isArray(arg)) {
+        result.push(...arg);
+      }
+      return result;
+    }, []);
+  };
+  
+  const result = concat('hi', [1, 2, 3], ['Hello', 'world']);
+  console.log(result); // Output: ['h', 'i', 1, 2, 3, 'Hello', 'world']
