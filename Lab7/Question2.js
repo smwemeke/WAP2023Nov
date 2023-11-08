e879fe2b1a840d6bb7fa8638137e7f828d5a1970
smwemeke <sue.mwemeke@gmail.com>
2023-11-08 17:59:44 -0600

    changes

---
+++ Lab7/Question2.js
@@ -0,0 +1,16 @@
+2. In the code below there’s a setTimeout call scheduled, then a heavy calculation is run, that takes
+more than 100ms to finish.
+When will the scheduled function run?
+1. After the loop.
+2. Before the loop.
+3. In the beginning of the loop.
+What is console.log going to show?
+let i = 0;
+setTimeout(() => console.log(i), 100); // ?
+// assume that the time to execute this function is >100ms
+for(let j = 0; j < 100000000; j++) {
+i++;
+}
+
+
+Answer: After the loop
